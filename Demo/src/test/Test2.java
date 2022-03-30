b73978ecb37680e92f00c50617f1f579490ac5dd
SnehaWani11 <snehawani11@gmail.com>
2022-03-30 11:46:08 +0530

    Done

---
+++ Demo/src/test/Test2.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		String test="Welcome to JAVA";
+		if(test.contains("JAVA"))
+		{
+			System.out.println("Found!!!");
+		}
+	}
+
+}
